cc8626a5df1b220de4956b9f580a60010662997d
jamesganglu <25brimwood@gmail.com>
2020-07-12 11:12:24 -0400

    style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { Link } from 'react-router-dom';
 import {toggleLogin} from '../actions/index';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <div className="container">
-          <span className="navbar-brand" href="#">boilderplate</span>
-
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item"> 
-                <Link to="/" className="nav-link">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/posts" className="nav-link">post list</Link>
-              </li>
-            </ul>
-
-            <Link to="/login" className="nav-link btn btn-primary">login</Link>
+      <>
+        <nav className="primary-nav">
+          <div className="container">
+            <div className="top-bar">
+              <div className="site-name"><span>AWS Practise</span></div>
+              <ul className="nav-links">
+                <li className="nav-item"> 
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/posts">post list</Link>
+                </li>
+                <li>
+                  <Link to="/login">login</Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </>
     )
   }
 }
